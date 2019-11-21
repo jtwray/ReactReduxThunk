5ee81b37573258dee45580b19e9f9faa9d492884
tucker <tuckerwray84@gmail.com>
2019-11-21 03:58:35 -0500

    add redux-logger. typo in App.js===> missing |'props'|.fetchKanyeQuote()

--- kanyeapi/src/App.js
+++ kanyeapi/src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { connect } from "react-redux"
+import { fetchKanyeQuote } from './actions'
 
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-
+      <button onClick={() => { props.fetchKanyeQuote() }}>share swagger</button>
+      {props.error && <div>{props.error}</div>}
+      {
+        (props.isLoading) ?
+          (<div>....loading swagggguh</div>) :
+            (<><div>the swaggggguh:{props.quote}</div></>)
+      }
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isLoading: state.isLoading, error: state.error, quote: state.quote,
+  }
+}
+export default connect(mapStateToProps, { fetchKanyeQuote })(App);
--- kanyeapi/src/index.js
+++ kanyeapi/src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux'
-import {Provider} from 'react-redux'
-import logger from "redux-logger"
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
-import rootReducer from './reducers'
+
+
+import fetchReducer from './reducers/fetchReducer'
 
 import './index.css';
 import App from './App';
- 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(fetchReducer, applyMiddleware(thunk,logger));
 
- 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+ const stores = createStore(fetchReducer,/* preloadedState, */ composeEnhancers(  applyMiddleware(thunk,logger)
+ ));
+
+ReactDOM.render(
+    <Provider store={stores}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+
+
