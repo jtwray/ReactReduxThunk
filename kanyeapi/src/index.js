123dcb3da37c4304d789ff08b8117adf7bdc0fd2
tucker <tuckerwray84@gmail.com>
2019-11-21 00:57:43 -0500

    Delete erroneous files

--- kanyeapi/src/index.js
+++ kanyeapi/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import logger from "redux-logger"
+
+import rootReducer from './reducers'
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+ 
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ 
