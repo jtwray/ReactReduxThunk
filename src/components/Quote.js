deedd80c5367b80997c55e2fc463b9a51fc65075
James 'Tucker' Wray <tuckerwray84@gmail.com>
2020-08-13 23:40:08 -0400

    Update Quote.js

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,47 +1,52 @@
 import React from "react";
-import { KnowledgeBomb } from "./KnowledgeBomb";
-import yeezy from "../images/kanyeSnippitNewYearsShades.PNG";
+
 import { connect } from "react-redux";
-import { fetchKanyeQuote } from "../actions/index";
+import { fetchKanyeQuote } from "../actions";
 import { useImageAPI } from "../hooks/useImageAPI";
 import "../App.css";
 
-const img1 = yeezy;
 const img2 = `https://image.tmdb.org/t/p/original/aiLF9OBQ7LcglmAg0pXjyonGwye.jpg`;
+
 const yeezybaby  = "https://i.imgur.com/3DB799e.png";
+
 const Quote = ({ quote, error, isLoading, fetchKanyeQuote, isCartoon }) => {
   const [kanyeImg, kanyeImgAlt] = useImageAPI(`kanye west`);
+
   return (
-    <>
-      {error && (<div>{error}</div>)({ isLoading }) ? (
-        <div>...its Comin`</div>
-      ) : (
-        <div className="quoteContainer grid">
-          {" "}
-           <div className="button--container">
-            <button onClick={() => fetchKanyeQuote()}>
-              drop a knowledge bomb <hr /> 💣
-            </button>
-          </div> 
-          {/* <div onClick={() => fetchKanyeQuote()}>
-            <KnowledgeBomb />
-          </div> */}
-          <div className="imageblock">
-            <img
-              src={isCartoon == true ? img2 : yeezybaby || kanyeImgAlt || img2}
-              styles={{ zIndex: "2" }}
-              alt="images of Kanye West"
-            />{" "}
-          </div>
-      
-          <div className="quoteblock">
-            <p onClick={() => fetchKanyeQuote()} className="quote">
-              {quote}
-            </p>
-          </div>
+    <div className="quoteContainer">
+      <div className="headerTitle">       
+        <h1> kany🎱</h1> 
+      </div>
+      <div className="Main">
+            {error && <div className="error">{error}</div>}
+
+            {isLoading && <div className="loading">...its Comin`</div>}
+
+            {
+              !error && !isLoading && 
+              <div className="quoteImgBtn">
+                  <div className="imgBtn">
+                    <img
+                      src={isCartoon === true ? yeezybaby :img2 ||  kanyeImgAlt || img2}
+                      styles={{ zIndex: "2" }}
+                      alt="images of Kanye West"
+                      />
+                    <button 
+                      onClick={() => fetchKanyeQuote()}>
+                      drop a knowledge bomb <hr /> 💣
+                    </button>
+                  </div>
+                  <div className="quote">
+                    <p 
+                      onClick={() => fetchKanyeQuote()}
+                      >{quote}
+                    </p>
+                  </div>         
+              </div>
+            }
         </div>
-      )}
-    </>
+      </div>
+      
   );
 };
 
@@ -50,7 +55,7 @@ const mapStateToProps = (state) => {
     quote: state.quote,
     error: state.error,
     isLoading: state.isLoading,
-    isCartoon: state.isCartoon,
+    isCartoon: state.isCartoon
   };
 };
 
